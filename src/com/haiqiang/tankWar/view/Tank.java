975eb8f3a5eb6f3d81330a4003dcdc681650e051
LikeBall <goodbye789@163.com>
2013-10-30 08:34:43 +0800

    0.7

---
+++ src/com/haiqiang/tankWar/view/Tank.java
@@ -0,0 +1,46 @@
+package com.haiqiang.tankWar.view;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+public class Tank {
+	
+	private int x;
+	private int y;
+	private int width;
+	private int heigth;
+	
+	Tank(int x, int y, int width, int heigth) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.heigth = heigth;
+	}
+	
+	public void draw(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.RED);
+		g.fillOval(x, y, width, heigth);
+		g.setColor(c);
+	}
+	
+	public void keyPerssed(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		switch(keyCode) {
+		case KeyEvent.VK_W : 
+			y -= 5;
+			break;
+		case KeyEvent.VK_S : 
+			y += 5;
+			break;
+		case KeyEvent.VK_A : 
+			x -= 5;
+			break;
+		case KeyEvent.VK_D : 
+			x += 5;
+			break;
+		default : ;
+		}
+	}
+}
